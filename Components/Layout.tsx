5c587f5b24b90a43b114b95c06c5613774db37a0
Nizam Uddin Babu <nizamcse.seu@gmail.com>
2021-11-03 20:06:05 +0600

    layout Problen Fixed

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './Header/Navbar';
 
-const Layout = ({children}) => {
+const Layout = ({children}:any) => {
     return (
         <div className='content'>
             <Navbar></Navbar>
