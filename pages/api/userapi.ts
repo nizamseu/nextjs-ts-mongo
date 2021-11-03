b44fa3e663ca917df4d8e03ab1ae6f7abb3832f8
Nizam Uddin Babu <nizamcse.seu@gmail.com>
2021-11-03 20:11:42 +0600

    all type is clear

--- pages/api/userapi.ts
+++ pages/api/userapi.ts
@@ -1,7 +1,7 @@
 import {connectToDatabase} from "../../lib/mongodb"
 
 
-export default async function handler(req,res){
+export default async function handler(req:any,res:any){
     
  const {db} = await connectToDatabase();
 const data = await db.collection("users").find({}).toArray();
