c15e875ae4da624606fac015426b1897f04cf044
unknown <nizamcse.seu@gmail.com>
2021-11-22 15:00:48 +0600

    start again

--- pages/updateUser.tsx
+++ pages/updateUser.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const updateUser = () => {
-    return (
-        <div>
-            <h1>Update</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Update</h1>
+    </div>
+  );
 };
 
 export default updateUser;
